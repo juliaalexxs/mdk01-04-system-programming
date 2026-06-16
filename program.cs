97cf1a220da817c440e304652921f89f01aae28d
juliaalexxs <juliaalexxs@yandex.ru>
2026-06-16 19:42:08 +0300

    создан массив на 100000 элементов с заполнением случ. чисел

--- program.cs
+++ program.cs
@@ -1,8 +1,20 @@
 using System;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 public class Program
 {
     public static void Main()
-    
+    {
+        int arr= 100000;
+        int[] numbers = new int[arr];
+            Random rnd = new Random();
+        for (int i = 0; i<arr; i++)
+        {
+            numbers[i] = rnd.Next(1, 101);
+        }
+
+
+        
+    }
 }
